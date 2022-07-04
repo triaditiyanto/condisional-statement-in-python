fcf0a38192ff044723dacc2f8b600cb82b50f730
Triaditiyanto <72344533+triaditiyanto@users.noreply.github.com>
2022-07-04 16:18:14 +0700

    Delete punya_adit.py

--- punya_adit.py
+++
@@ -1,6 +0,0 @@
-print("== TEBAK AYANG ADIT ==")
-nama = input("Masukan Nama Kamu : ")
-
-if nama=="kezia" : print("=== kamu ayang aditt!!! <3<3 ==="), print("=== I Love Youuu!!! ==="), print("=== Aku sayang Kamuuu!!!  ===")  
-
-else : print("== !!Kamu bukan ayang adit!! ==")
